55e9b94134d677956df00d26d312b0920d605402
chitra <chmanu.chinu@gmail.com>
2023-03-09 09:28:15 +0000

    update learn-shell

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,6 +1,11 @@
 DATE=2023-03-09
 echo "Hello,Good Morning,Today Date is ${DATE}"
 
-
+# Command substitution
 DATE=$(date)
 echo "Hello,Good Morning,Today Date is ${DATE}"
+
+
+# Arthimetic substitution
+ADD=$((2+3))
+"echo Addition of 2+3 = ${ADD}"
