211bbd62df2f883b99b88145001d3664b7fc64ca
chitra <chmanu.chinu@gmail.com>
2023-03-10 08:41:13 +0000

    update learn-shell

---
+++ 06-conditions.sh
@@ -0,0 +1,9 @@
+numbeer $1
+string $2
+
+if [${number} -eq 5 ]; then # then can go in next line as well,also semi colon is alo fine in same line
+echo number is 5
+fi
+
+if [ ${string} ==abc ]; then
+echo string is abc
